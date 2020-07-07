b510800c8d58778d21a2a4ca4a7ed7a6840d40ec
lizhen3250 <764482832@qq.com>
2020-07-08 00:16:26 +0800

    golf admin

---
+++ QD-Tour-Admin/QD-Tour-Admin/Controllers/GolfController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QD_Tour_Admin.Model;
+
+namespace QD_Tour_Admin.Controllers
+{
+    public class GolfController : Controller
+    {
+        private QDTourAdminEntities db = new QDTourAdminEntities();
+
+        // GET: Golf
+        public ActionResult Index()
+        {
+            var golfs = db.Golves.ToList();
+
+            return View(golfs);
+        }
+    }
+}
