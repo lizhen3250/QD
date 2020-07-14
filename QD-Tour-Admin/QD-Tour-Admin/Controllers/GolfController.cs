92eb6c011c952f48a38b4740010bab6ea8d289a7
lizhen3250 <764482832@qq.com>
2020-07-14 09:20:01 +0800

    Golf management

--- QD-Tour-Admin/QD-Tour-Admin/Controllers/GolfController.cs
+++ QD-Tour-Admin/QD-Tour-Admin/Controllers/GolfController.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using QD_Tour_Admin.Model;
+using QD_Tour_Admin.Service;
 
 namespace QD_Tour_Admin.Controllers
 {
+    public class GolfModel
+    {
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    [CustomAuthorize]
     public class GolfController : Controller
     {
         private QDTourAdminEntities db = new QDTourAdminEntities();
@@ -17,6 +27,28 @@ namespace QD_Tour_Admin.Controllers
             var golfs = db.Golves.ToList();
 
             return View(golfs);
+        }
+
+        [HttpPost]
+        public string Add(GolfModel golf)
+        {
+            Golf newGolf = new Golf()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = golf.Name,
+                City = golf.City,
+                Address = golf.Address
+
+            };
+
+            db.Golves.Add(newGolf);
+
+            if (db.SaveChanges() > 0)
+            {
+                return "添加成功";
+            }
+
+            return "添加失败";
         }
     }
 }
