e6699eef079e0e34cd732ed12881abc32db65e74
lizhen3250 <764482832@qq.com>
2020-07-14 20:31:51 +0800

    implemented golf web index page

---
+++ QD-Tour-Web/QD-Tour-Web/Controllers/GolfController.cs
@@ -0,0 +1,20 @@
+﻿using QD_Tour_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QD_Tour_Web.Controllers
+{
+    public class GolfController : Controller
+    {
+        private QDTourWebEntities db = new QDTourWebEntities();
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            var golfPackages = db.Golf_Package.ToList();
+            return View(golfPackages);
+        }
+    }
+}
